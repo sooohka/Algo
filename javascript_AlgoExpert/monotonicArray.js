2793c4287c6e0ccdf2c7e112b8d50121e6869bc3
강수호(sooohka) <shkorea1004@gmail.com>
2022-03-07 23:42:01 +0900

    algo: monotonicArray
    https://www.algoexpert.io/questions/Monotonic%20Array

---
+++ javascript_AlgoExpert/monotonicArray.js
@@ -0,0 +1,13 @@
+function isMonotonic(array) {
+  if (array.length < 2) return true;
+  let status = "unknown"; //"unknown", "increasing", "decreasing"
+  for (let i = 1; i < array.length; i += 1) {
+    if (status === "unknown") {
+      if (array[i - 1] < array[i]) status = "increasing";
+      else if (array[i - 1] > array[i]) status = "decreasing";
+    }
+    if (status === "decreasing" && array[i - 1] < array[i]) return false;
+    if (status === "increasing" && array[i - 1] > array[i]) return false;
+  }
+  return true;
+}
