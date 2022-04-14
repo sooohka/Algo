eb8ab33ac65a8431a5635c7efef7910f8432af9e
강수호(sooohka) <shkorea1004@gmail.com>
2022-04-14 15:21:02 +0900

    feat:breadthFirstSearch
    https://www.algoexpert.io/questions/Breadth-first%20Search

---
+++ javascript_AlgoExpert/breadthFirstSearch.js
@@ -0,0 +1,30 @@
+// Do not edit the class below except
+// for the breadthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  breadthFirstSearch(array) {
+    const stack = [this];
+    while (stack.length !== 0) {
+      const cur = stack.shift();
+      for (const child of cur.children) {
+        stack.push(child);
+      }
+      array.push(cur.name);
+    }
+    return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
